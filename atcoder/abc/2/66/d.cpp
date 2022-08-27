244613a15f7fce0f7fef173e614bcfe4639015df
u-sho <26079835+u-sho@users.noreply.github.com>
2022-08-27 23:09:39 +0900

    abc266 d practice: fixed overflow

--- atcoder/abc/2/66/d.cpp
+++ atcoder/abc/2/66/d.cpp
@@ -1,26 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ui = unsigned;
-using lli = long long;
 using ulli = unsigned long long;
 
 #define ALL(v) (v).begin(), (v).end()
 
-#define REP(i, n) for (ui i = 0UL; i < (n); i++)
-#define REP1(i, n) for (ui i = 1UL; i < (n); i++)
-
-ui d(const ui &a, const ui &b) { return a > b ? a - b : b - a; }
+#define REP(i, n) for (ulli i = 0UL; i < (n); i++)
+#define REP1(i, n) for (ulli i = 1UL; i < (n); i++)
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    ui N;
+    ulli N;
     cin >> N;
-    vector<ui> T, X, A;
+    vector<ulli> T, X, A;
     REP(i, N) {
-        ui Ti, Xi, Ai;
+        ulli Ti, Xi, Ai;
         cin >> Ti >> Xi >> Ai;
         T.push_back(Ti);
         X.push_back(Xi);
@@ -49,13 +45,13 @@ int main() {
     REP1(i, N) {
         // REP(j, 5) cout << dp[i - 1][j] << ' ';
         // cout << '\n';
-        const ui dT = T[i] - T[i - 1];
+        const ulli dT = T[i] - T[i - 1];
         REP(j, 5) {
             if (T[i] < j) break;
-            const ui b = (dT < j) ? (j - dT) : 0;
-            const ui e = (j + dT < 5U) ? (4U - j - dT) : 0;
-            const ui maxTi1 = *max_element(next(dp[i - 1].begin(), b),
-                                           prev(dp[i - 1].end(), e));
+            const ulli b = (dT < j) ? (j - dT) : 0;
+            const ulli e = (j + dT < 5U) ? (4U - j - dT) : 0;
+            const ulli maxTi1 = *max_element(next(dp[i - 1].begin(), b),
+                                             prev(dp[i - 1].end(), e));
             dp[i][j] = maxTi1;
             if (X[i] == j) {
                 dp[i][j] += A[i];
