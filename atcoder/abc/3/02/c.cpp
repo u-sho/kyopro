813d89b0b1014b6e71825dc25a62a338db067e2c
u-sho <shouhei.uechi@gmail.com>
2023-06-19 16:48:12 +0900

    abc302 c; practice

--- atcoder/abc/3/02/c.cpp
+++ atcoder/abc/3/02/c.cpp
@@ -2,40 +2,40 @@
 using namespace std;
 
 #define ALL(v) (v).begin(), (v).end()
-#define SORT(v) sort(ALL(v))
 
-#define REP(i, n) for (int i = 0; i < (n); i++)
+#define REP(i, n) for (unsigned i = 0; i < (n); i++)
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    int N, M;
+    unsigned N, M;
     cin >> N >> M;
 
     vector<string> S(N);
-    for (string& Si : S) {
-        cin >> Si;
-        SORT(Si);
-        Si.push_back(Si.front());  // XXX: 1つshiftして数える用
-    }
+    for (string& Si : S) cin >> Si;
 
-    // diff_S[i][j]: S[i] と S[j] が何文字違うのか数える．
-    vector<vector<unsigned>> diff_S(N, vector<unsigned>(N, 0));
-    REP(i, N) {
-        REP(j, N) {
-            unsigned diff_counter         = 0;
-            unsigned diff_counter_shift_l = 0;
-            unsigned diff_counter_shift_r = 0;
-            REP(k, M) {
-                diff_counter         += (S[i][k] != S[j][k]);
-                diff_counter_shift_l += (S[i][k] != S[j][k + 1]);
-                diff_counter_shift_r += (S[i][k + 1] != S[j][k]);
+    // for next_permutation
+    sort(ALL(S));
+
+    do {
+        /* 条件を満たしているか */
+        bool is_satisfied = true;
+        REP(i, N - 1) {
+            unsigned diff_counter = 0;
+            REP(j, M) { diff_counter += (S[i][j] != S[i + 1][j]); }
+            if (diff_counter > 1U) {
+                is_satisfied = false;
+                break;
             }
-            diff_S[i][j] = min(diff_counter,
-                               min(diff_counter_shift_l, diff_counter_shift_r));
         }
-    }
+        if (is_satisfied) {
+            cout << "Yes\n";
+            return 0;
+        }
+    } while (next_permutation(ALL(S)));
+
+    cout << "No\n";
 
     return 0;
 }
