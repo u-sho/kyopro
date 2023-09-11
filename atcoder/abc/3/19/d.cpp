59887d6886caf5325c6fbfe930db799ae9082c3d
u-sho <shouhei.uechi@gmail.com>
2023-09-11 11:29:37 +0900

    二分探索ライブラリを追加

--- atcoder/abc/3/19/d.cpp
+++ atcoder/abc/3/19/d.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ALL(v) (v).begin(), (v).end()
-
-#define REP(i, n)  for (size_t i = 0UL; i < (n); i++)
-#define REP1(i, n) for (size_t i = 1UL; i < (n); i++)
-
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
@@ -15,26 +10,26 @@ int main() {
     vector<uint_fast32_t> L(N);
     for (uint_fast32_t& Li : L) cin >> Li;
 
-    uint_fast64_t l = *max_element(ALL(L));
-    uint_fast64_t r = reduce(ALL(L), uint_fast64_t(N - 1U));  // 空白が N-1 個
-    while (l < r) {
-        uint_fast64_t c = l + (r - l) / 2U;
+    const auto maxL = uint_fast64_t(ranges::max(L));
+    const auto maxW = reduce(L.begin(), L.end(), uint_fast64_t(N - 1U));
 
-        uint_fast32_t line   = 1;
-        uint_fast64_t cursor = L.front();
-        REP1(i, N) {
-            if (cursor + 1U + L[i] <= c) cursor += 1U + L[i];
-            else {
-                ++line;
-                cursor = L[i];
+    // 画面幅が W のとき，M 行を超えるか
+    const auto is_over_M = [&L, &M](const uint_fast64_t W) -> bool {
+        uint_fast32_t lines  = 1U;
+        uint_fast64_t cursor = 0U;
+        for (const auto& Li : L) {
+            if (cursor + Li > W) {  // 画面幅を超えるなら改行
+                ++lines;
+                cursor = 0U;
             }
+            cursor += Li + 1U;  // 行末に単語（＋空白）を追加
         }
+        return (lines > M);
+    };
 
-        if (line <= M) r = c;
-        else l = c + 1U;
-    }
-
-    cout << r << '\n';
+    const auto minW =
+        ranges::partition_point(views::iota(maxL, maxW), is_over_M);
+    cout << *minW << '\n';
 
     return 0;
 }
