f5a9549f2a5286d74a594a45bc46e6aea4c402eb
u-sho <26079835+u-sho@users.noreply.github.com>
2023-10-29 16:21:14 +0900

    c++20 -> c++23

--- atcoder/abc/3/26/d.cpp
+++ atcoder/abc/3/26/d.cpp
@@ -1,202 +1,105 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <algorithm>
+#include <iostream>
+#include <string>
 
-#define SORT(v)    std::ranges::sort(v)
-#define ALL(v)     (v).begin(), (v).end()
-#define REP(i, n)  for (size_t i = 0UL; i < (n); i++)
-#define REP1(i, n) for (size_t i = 1UL; i < (n); i++)
+#define NP(A, i)  std::next_permutation(A[i].begin(), A[i].end())
+#define REP(i, n) for (uint i = 0U; i < (n); i++)
+
+uint N;
+std::string R, C;
+std::string A[5Z] = {".....", ".....", ".....", ".....", "....."};
+
+// A[i]の先頭がR[i]か
+bool is_A_ok_R(uint i) {
+    REP(j, N) {
+        if (A[i][j] == '.') continue;
+        if (A[i][j] == R[i]) return true;
+        else return false;
+    }
+    return false;
+}
+
+// 列の先頭が C[i] か
+bool is_A_ok_C() {
+    REP(j, N) {
+        REP(i, N) {
+            if (A[i][j] == '.') continue;
+            if (A[i][j] == C[j]) break;
+            else return false;
+        }
+    }
+    return true;
+}
+
+// 列にABC一つずつか
+bool do_columns_satisfy() {
+    REP(j, N) {
+        bool cA = false, cB = false, cC = false;
+        REP(i, N) {
+            if (A[i][j] == 'A') {
+                if (cA) return false;
+                else cA = true;
+            }
+            if (A[i][j] == 'B') {
+                if (cB) return false;
+                else cB = true;
+            }
+            if (A[i][j] == 'C') {
+                if (cC) return false;
+                else cC = true;
+            }
+        }
+        if (!cA || !cB || !cC) return false;
+    }
+    return true;
+}
 
 int main() {
+    using std::cin;
+    using std::cout;
     cin.tie(nullptr);
-    ios::sync_with_stdio(false);
+    std::ios::sync_with_stdio(false);
 
-    uint N;
     cin >> N;
-    string R;
     cin >> R;
-    string C;
     cin >> C;
 
+    // 行にABC一つずつ
+    REP(i, N) {
+        A[i].resize(N);
+        A[i][N - 3U] = 'A';
+        A[i][N - 2U] = 'B';
+        A[i][N - 1U] = 'C';
+    }
+
     // 全探索
-    if (N == 3U) {
-        vector<string> A = {"...", "...", "..."};
-        if (R[0] != C[0]) cout << "No\n";
-        else if (R[0] == R[1] || R[1] == R[2] || R[2] == R[0]) cout << "No\n";
-        else if (C[0] == C[1] || C[1] == C[2] || C[2] == C[0]) cout << "No\n";
-        else {
-            A[0]    = C;
-            A[1][0] = R[1];
-            A[2][0] = R[2];
-            if (R[1] == C[1]) {
-                A[1][1] = R[2];
-                A[2][2] = R[1];
-                A[1][2] = A[2][1] = R[0];
-            } else {
-                A[1][2] = R[2];
-                A[2][1] = R[1];
-                A[1][1] = A[2][2] = R[0];
-            }
-            cout << "Yes\n";
-            cout << A[0] << "\n" << A[1] << "\n" << A[2] << "\n";
-        }
-        return 0;
-    }
+    do {  // 5! = 120
+        // R[0]が先頭：5C2 * 2! = 20
+        if (!is_A_ok_R(0U)) continue;
+        do {
+            if (!is_A_ok_R(1U)) continue;
+            do {
+                if (!is_A_ok_R(2U)) continue;
+                do {
+                    if (N > 3U)
+                        if (!is_A_ok_R(3U)) continue;
+                    do {
+                        if (N > 4U)
+                            if (!is_A_ok_R(4U)) continue;
 
-    if (N == 4U) {
-        vector<string> A(N, ".ABC");
-        vector<bool> checked_C(N, false);
-        do {              // 4! = 24
-            do {          // 4! = 24
-                do {      // 4! = 24
-                    do {  // 4! = 24
-                        for (uint j = 0; j < N; j++) {
-                            bool cA = false, cB = false, cC = false;
-                            for (uint i = 0; i < N; i++) {
-                                if (A[i][j] == 'A') {
-                                    if (cA) goto LABEL;
-                                    else cA = true;
-                                }
-                                if (A[i][j] == 'B') {
-                                    if (cB) goto LABEL;
-                                    else cB = true;
-                                }
-                                if (A[i][j] == 'C') {
-                                    if (cC) goto LABEL;
-                                    else cC = true;
-                                }
-                            }
-                            if (!(cA && cB && cC)) goto LABEL;
-                        }
+                        if (!do_columns_satisfy()) continue;
+                        if (!is_A_ok_C()) continue;
 
-                        for (uint i = 0; i < N; i++) checked_C[i] = false;
-                        for (uint i = 0; i < N; i++) {
-                            bool checked_R = false;
-                            for (uint j = 0; j < N; j++) {
-                                if (A[i][j] == '.') continue;
-                                if (!checked_R) {
-                                    if (A[i][j] == R[i]) checked_R = true;
-                                    else goto LABEL;
-                                }
-                                if (!checked_C[j]) {
-                                    if (A[i][j] == C[j]) checked_C[j] = true;
-                                    else goto LABEL;
-                                }
-                            }
-                        }
-                        for (uint i = 0; i < N; i++) {
-                            if (!checked_C[i]) goto LABEL;
-                        }
                         cout << "Yes\n";
-                        cout << A[0] << "\n";
-                        cout << A[1] << "\n";
-                        cout << A[2] << "\n";
-                        cout << A[3] << "\n";
+                        REP(i, N) cout << A[i] << "\n";
                         return 0;
-                    LABEL:
-                        continue;
-                    } while (next_permutation(ALL(A[3])));
-                } while (next_permutation(ALL(A[2])));
-            } while (next_permutation(ALL(A[1])));
-        } while (next_permutation(ALL(A[0])));
-    }
 
-    if (N == 5U) {
-        vector<string> A(N, "..ABC");
-        vector<bool> checked_C(N, false);
-        do {  // 5! = 120
-              // A[0]の先頭がR[0]か：20通り
-            for (uint i = 0; i < N; i++) {
-                if (A[0][i] == '.') continue;
-                if (A[0][i] == R[0]) break;
-                else goto CONTINUE_0;
-            }
-            do {  // 5! = 120 : 20通り
-                for (uint i = 0; i < N; i++) {
-                    if (A[1][i] == '.') continue;
-                    if (A[1][i] == R[1]) break;
-                    else goto CONTINUE_1;
-                }
-                do {  // 5! = 120 : 20通り
-                    for (uint i = 0; i < N; i++) {
-                        if (A[2][i] == '.') continue;
-                        if (A[2][i] == R[2]) break;
-                        else goto CONTINUE_2;
-                    }
-                    do {  // 5! = 120 : 20通り
-                        for (uint i = 0; i < N; i++) {
-                            if (A[3][i] == '.') continue;
-                            if (A[3][i] == R[3]) break;
-                            else goto CONTINUE_3;
-                        }
-                        do {  // 5! = 120 : 20通り
-                            for (uint i = 0; i < N; i++) {
-                                if (A[4][i] == '.') continue;
-                                if (A[4][i] == R[4]) break;
-                                else goto G_CONTINUE;
-                            }
-
-                            // 列にABC一つずつか
-                            for (uint j = 0; j < N; j++) {
-                                bool cA = false, cB = false, cC = false;
-                                for (uint i = 0; i < N; i++) {
-                                    if (A[i][j] == 'A') {
-                                        if (cA) goto G_CONTINUE;
-                                        else cA = true;
-                                    }
-                                    if (A[i][j] == 'B') {
-                                        if (cB) goto G_CONTINUE;
-                                        else cB = true;
-                                    }
-                                    if (A[i][j] == 'C') {
-                                        if (cC) goto G_CONTINUE;
-                                        else cC = true;
-                                    }
-                                }
-                                if (!(cA && cB && cC)) goto G_CONTINUE;
-                            }
-
-                            // 列の先頭が C[i] か
-                            for (uint i = 0; i < N; i++) checked_C[i] = false;
-                            for (uint i = 0; i < N; i++) {
-                                for (uint j = 0; j < N; j++) {
-                                    if (A[i][j] == '.') continue;
-                                    if (!checked_C[j]) {
-                                        if (A[i][j] == C[j])
-                                            checked_C[j] = true;
-                                        else goto G_CONTINUE;
-                                    }
-                                }
-                            }
-                            for (uint i = 0; i < N; i++) {
-                                if (!checked_C[i]) goto G_CONTINUE;
-                            }
-
-                            cout << "Yes\n";
-                            cout << A[0] << "\n";
-                            cout << A[1] << "\n";
-                            cout << A[2] << "\n";
-                            cout << A[3] << "\n";
-                            cout << A[4] << "\n";
-                            return 0;
-
-                        G_CONTINUE:
-                            continue;
-                        } while (next_permutation(ALL(A[4])));
-                    CONTINUE_3:
-                        continue;
-                    } while (next_permutation(ALL(A[3])));
-                CONTINUE_2:
-                    continue;
-                } while (next_permutation(ALL(A[2])));
-            CONTINUE_1:
-                continue;
-            } while (next_permutation(ALL(A[1])));
-        CONTINUE_0:
-            continue;
-        } while (next_permutation(ALL(A[0])));
-    }
+                    } while (NP(A, 4Z));
+                } while (NP(A, 3Z));
+            } while (NP(A, 2Z));
+        } while (NP(A, 1Z));
+    } while (NP(A, 0Z));
 
     cout << "No\n";
-
     return 0;
 }
