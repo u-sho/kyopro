80845ee1806383195bdcb8488dc08a087c34df53
u-sho <26079835+u-sho@users.noreply.github.com>
2024-04-21 01:42:23 +0900

    abc350 d; 解説AC

--- atcoder/abc/3/50/f.cpp
+++ atcoder/abc/3/50/f.cpp
@@ -1,41 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <atcoder/all>
-using namespace atcoder;
+string S;
 
-#define SORT(v)  std::ranges::sort(v)
-#define ALL(v)   (v).begin(), (v).end()
-#define LP(v, c) lower_bound(ALL(v), (c))
-#define UP(v, c) upper_bound(ALL(v), (c))
-#define BS(v, c) binary_search(ALL(v), (c))
+/** parens[i]: S[i]==')' に対応する '(' の位置 または その逆 */
+vector<uint> parens;
 
-#define REP(i, n)  for (size_t i = 0UL; i < (n); i++)
-#define REP1(i, n) for (size_t i = 1UL; i < (n); i++)
+/** f(l,r,depth): depthの偶奇に応じ S[l,r] を（順に or 反転して）出力する */
+void f(uint l, uint r, bool is_odd_depth = false) {
+    if (is_odd_depth) {
+        for (uint i = r; i >= l; i--) {
+            if (S[i] == ')') {
+                f(parens[i] + 1U, i - 1U, false);
+                i = parens[i];
+            } else {
+                cout << S[i];
+            }
+        }
+        return;
+    }
+
+    for (uint i = l; i <= r; i++) {
+        if (S[i] == '(') {
+            f(i + 1U, parens[i] - 1U, true);
+            i = parens[i];
+        } else {
+            cout << S[i];
+        }
+    }
+    return;
+}
 
 int main() {
     cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    string S;
     cin >> S;
-    uint n     = S.length();
+    const uint n = S.length();
+
     uint depth = 0;
-    vector<uint> lparen, rparen;
+    stack<uint> lparen;
+    parens.resize(n);
     for (uint i = 0; i < n; i++) {
         if (S[i] == '(') {
             ++depth;
-            lparen.push_back(i);
+            lparen.push(i);
         } else if (S[i] == ')') {
             --depth;
-            rparen.push_back(i);
-        } else {
-            if (depth & 1U) {
-                if (S[i] <= 'Z') S[i] += 'a' - 'A';
-                else S[i] -= 'a' - 'A';
-            }
+            parens[i] = lparen.top();
+            lparen.pop();
+            parens[parens[i]] = i;
+        } else if (depth & 1U) {
+            if (S[i] <= 'Z') S[i] += 'a' - 'A';
+            else S[i] -= 'a' - 'A';
         }
     }
+
+    f(0U, n - 1U);
 
     return 0;
 }
