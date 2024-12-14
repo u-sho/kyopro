b93e0bb15d52484689344fc3bb906225a1e1a010
u-sho <26079835+u-sho@users.noreply.github.com>
2024-12-15 00:18:48 +0900

    fetch to Library

--- atcoder/abc/3/84/c.cpp
+++ atcoder/abc/3/84/c.cpp
@@ -1,52 +1,49 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <set>
 
-using Participant = pair<uint, string>;
+#define BitDP(flag, value) \
+    for (decltype(value) flag = 0; flag <= (value); flag += (value))
+
+using score_t       = uint;
+using name_t        = std::string;
+using participant_t = std::pair<score_t, name_t>;
 
 struct comp {
-    template <typename T>
+    template <class T = participant_t>
     bool operator()(const T &l, const T &r) const {
-        if (l.first == r.first) {
-            return l.second > r.second;
-        }
+        // 点数が同じなら辞書順
+        if (l.first == r.first) return l.second > r.second;
 
+        // 基本は点数順
         return l.first < r.first;
     }
 };
 
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
 
-    uint abcde[5];
-    for (auto &point : abcde) cin >> point;
+    score_t a, b, c, d, e;
+    std::cin >> a >> b >> c >> d >> e;
+    std::set<participant_t, comp> participants;
 
-    set<Participant, comp> participants;
+    BitDP(A, a) BitDP(B, b) BitDP(C, c) BitDP(D, d) BitDP(E, e) {
+        const score_t score = A + B + C + D + E;
+        name_t solved       = "";
+        if (A) solved.push_back('A');
+        if (B) solved.push_back('B');
+        if (C) solved.push_back('C');
+        if (D) solved.push_back('D');
+        if (E) solved.push_back('E');
 
-    for (uint a = 0; a <= abcde[0]; a += abcde[0]) {
-        for (uint b = 0; b <= abcde[1]; b += abcde[1]) {
-            for (uint c = 0; c <= abcde[2]; c += abcde[2]) {
-                for (uint d = 0; d <= abcde[3]; d += abcde[3]) {
-                    for (uint e = 0; e <= abcde[4]; e += abcde[4]) {
-                        const uint score = a + b + c + d + e;
-                        string solved    = "";
-                        if (a) solved.push_back('A');
-                        if (b) solved.push_back('B');
-                        if (c) solved.push_back('C');
-                        if (d) solved.push_back('D');
-                        if (e) solved.push_back('E');
-
-                        participants.insert({score, solved});
-                    }
-                }
-            }
-        }
+        participants.insert({score, solved});
     }
 
-    const uint sum = reduce(abcde, abcde + 5, 0U);
-    auto it        = participants.lower_bound({sum, "ABCDE"});
+    const participant_t first_participant = {a + b + c + d + e, "ABCDE"};
+
+    auto it = participants.lower_bound(first_participant);
     for (uint i = 0; i < 31; ++i) {
-        cout << it->second << '\n';
+        std::cout << it->second << '\n';
         --it;
     }
 
