696c23e2ff5ab84e09a3c5cdb261918e5ee9a9e8
u-sho <26079835+u-sho@users.noreply.github.com>
2025-05-25 01:31:40 +0900

    abc407 d; practice

--- atcoder/abc/4/07/d.cpp
+++ atcoder/abc/4/07/d.cpp
@@ -3,34 +3,41 @@ using namespace std;
 
 uint H, W;
 vector<uint64_t> A(30);
-uint64_t solve(const uint k, uint64_t sum,
-               vector<bool> d = vector<bool>(30, false), const uint ij = 0) {
+uint64_t solve(const uint k, uint64_t sum, uint32_t covered = 0u,
+               const uint ij = 0) {
+    auto is_covered = [&](const uint ij) { return !!(covered & (0b1 << ij)); };
+
+    // 範囲外
     if (ij >= H * W - 1) return sum;
 
-    const uint i = ij / W;
-    const uint j = ij % W;
+    // 置きたいけど置けない
+    if (k && is_covered(ij)) return sum;
+    if (k == 1 && (W == 1 || ij % W == W - 1 || is_covered(ij + 1))) return sum;
+    if (k == 2 && (H == 1 || ij + W >= H * W || is_covered(ij + W))) return sum;
 
-    // 横置き（右端にいないかつマスが埋まっていないとき）
-    if (k == 1 && j < W - 1 && !d[ij] && !d[ij + 1]) {
-        d[ij]      = true;
-        d[ij + 1]  = true;
-        sum       ^= A[ij] ^ A[ij + 1];
-        return max(solve(0, sum, d, ij + 2),
-                   max(solve(1, sum, d, ij + 2), solve(2, sum, d, ij + 2)));
+    // 横置き
+    if (k == 1) {
+        covered ^= 0b11 << ij;
+        sum     ^= A[ij] ^ A[ij + 1];
+        return max(solve(0, sum, covered, ij + 2),
+                   max(solve(1, sum, covered, ij + 2),
+                       solve(2, sum, covered, ij + 2)));
     }
 
     // 縦置き
-    if (k == 2 && ij + W <= H * W - 1 && !d[ij] && !d[ij + W]) {
-        d[ij]      = true;
-        d[ij + W]  = true;
-        sum       ^= A[ij] ^ A[ij + W];
-        return max(solve(0, sum, d, ij + 1),
-                   max(solve(1, sum, d, ij + 1), solve(2, sum, d, ij + 1)));
+    if (k == 2) {
+        covered ^= 0b1 << ij;
+        covered ^= 0b1 << (ij + W);
+        sum     ^= A[ij] ^ A[ij + W];
+        return max(solve(0, sum, covered, ij + 1),
+                   max(solve(1, sum, covered, ij + 1),
+                       solve(2, sum, covered, ij + 1)));
     }
 
-    // その他
-    return max(solve(0, sum, d, ij + 1),
-               max(solve(1, sum, d, ij + 1), solve(2, sum, d, ij + 1)));
+    // 置かない
+    return max(
+        solve(0, sum, covered, ij + 1),
+        max(solve(1, sum, covered, ij + 1), solve(2, sum, covered, ij + 1)));
 }
 
 int main() {
@@ -46,7 +53,6 @@ int main() {
     }
 
     // bit-DP は間に合わない<-嘘．間に合う
-    // DP でいい感じに範囲を絞ってドミノを置く
     // （ドミノで (i,j) が埋まると sum ^= A[i][j] になる）
 
     uint64_t ans = max(solve(0, sum), max(solve(1, sum), solve(2, sum)));
