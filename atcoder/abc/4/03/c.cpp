44a498c642139f6438a78496878461b593f1e806
u-sho <26079835+u-sho@users.noreply.github.com>
2025-07-07 00:36:21 +0900

    abc403 c; 解決 TLEはたぶんMLE

--- atcoder/abc/4/03/c.cpp
+++ atcoder/abc/4/03/c.cpp
@@ -1,71 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define REP(i, n)  for (size_t i = 0UL; i < (n); i++)
-#define REP1(i, n) for (size_t i = 1UL; i < (n); i++)
-
-// template <typename T>
-// std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-//     for (std::size_t i = 0; i < v.size(); i++) {
-//         os << v[i] << (i + 1UL != v.size() ? "" : "\n");  // 空白区切り
-//         // os << v[i] << "\n";  // 改行区切り
-//     }
-//     return os;
-// }
-
 int main() {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
 
-    uint N, M, Q;
-    cin >> N >> M >> Q;
+	uint N, M, Q;
+	cin >> N >> M >> Q;
 
-    bitset<200'000> ALL_PAGES_USER_CAN_VIEW;
-    for (uint i = 0; i < M; i++) {
-        ALL_PAGES_USER_CAN_VIEW[i].flip();
-    }
+	set<pair<uint, uint>> can_view;
+	for (uint q = 0; q < Q; q++) {
+		uint query_type, X, Y;
+		cin >> query_type;
+		if (query_type == 1) {
+			cin >> X >> Y;
+			can_view.emplace(X, Y);
+		}
+		if (query_type == 2) {
+			cin >> X;
+			can_view.emplace(X, 0);
+		}
+		if (query_type == 3) {
+			cin >> X >> Y;
+			if (can_view.contains({X, 0}) || can_view.contains({X, Y})) {
+				cout << "Yes\n";
+			} else {
+				cout << "No\n";
+			}
+		}
+	}
 
-    vector<bitset<200'000>> pages_user_can_view(N);
-
-    REP(q, Q) {
-        uint query;
-        cin >> query;
-        if (query == 1) {
-            uint X, Y;
-            cin >> X >> Y;
-            pages_user_can_view[X - 1][Y - 1] = true;
-            continue;
-        }
-        if (query == 2) {
-            uint X;
-            cin >> X;
-            pages_user_can_view[X - 1] |= ALL_PAGES_USER_CAN_VIEW;
-            continue;
-        }
-        if (query == 3) {
-            uint X, Y;
-            cin >> X >> Y;
-            cout << (pages_user_can_view[X - 1][Y - 1] ? "Yes\n" : "No\n");
-        }
-        // cout << X << ": " << pages_user_can_view[X - 1];
-
-        // switch (query) {
-        //     case 1:
-        //         cin >> Y;
-        //         pages_user_can_view[X - 1][Y - 1] = true;
-        //         break;
-        //     case 2:
-        //         pages_user_can_view[X - 1] |= ALL_PAGES_USER_CAN_VIEW;
-        //         break;
-        //     case 3:
-        //         cin >> Y;
-        //         cout << (pages_user_can_view[X - 1][Y - 1] ? "Yes\n" :
-        //         "No\n"); break;
-        //     default:
-        //         assert(-1);
-        //         break;
-        // }
-    }
-
-    return 0;
+	return 0;
 }
