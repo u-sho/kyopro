66d6183521d856b05260d346ad08cd47e3553be8
u-sho <26079835+u-sho@users.noreply.github.com>
2025-07-10 08:38:51 +0900

    abc129 c; 解説ac

--- atcoder/abc/1/29/c.cpp
+++ atcoder/abc/1/29/c.cpp
@@ -6,19 +6,13 @@ int main(){
 	vector<uint> a(M);
 	for (uint& ai : a) cin >> ai;
 
-	vector<uint> steps(N+2) = {0, 1, 2};
+	vector<uint> steps(N+1, 1);
+	for (uint j = 0; j < M; j++) steps[a[j]] = 0;
 	for (uint i = 2; i <= N; i++) {
+		if (steps[i] == 0) continue;
 		steps[i] = (steps[i - 1] + steps[i - 2]) % 1000'000'007u;
 	}
-	if (M == 0) {
-		cout << steps[N] << endl;
-		return 0;
-	}
-	for (uint j = 0; j < M; j++) {
-		steps[a[j]] = 0;
-	}
-	for (uint i = 2; i <= N; i++) {
-		steps[i] = (steps[i - 1] + 
-	}
+
+	cout << steps[N] << endl;
 	return 0;
 }
