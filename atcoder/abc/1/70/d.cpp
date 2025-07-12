d84297f28f601a6c2e011559668b77de71f73f4e
u-sho <26079835+u-sho@users.noreply.github.com>
2025-07-13 01:31:42 +0900

    abc170 d; practice

--- atcoder/abc/1/70/d.cpp
+++ atcoder/abc/1/70/d.cpp
@@ -1,39 +1,34 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-using std::make_pair;
-using std::pair;
-
-using std::max;
-using std::min;
-using std::sort;
-
-using std::cin;
-using std::cout;
-
-using std::vector;
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) cin >> A.at(i);
-    sort(A.begin(), A.end());
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
 
-    vector<bool> warikireru(1000006);
-    std::fill(warikireru.begin(), warikireru.end(), false);
-    warikireru.at(1) = true;
+	uint N;
+	cin >> N;
+	vector<uint> A(N);
+	for (uint& Ai : A) cin >> Ai;
+	ranges::sort(A);
 
-    int ans = 0;
-    for (int i = 0; i < N; ++i) {
-        if (warikireru.at(A.at(i))) continue;
-        for (int j = 1; j * A.at(i) <= 1000000; ++j) {
-            warikireru.at(j * A.at(i)) = true;
-        }
-        if (i == N - 1 || A.at(i) != A.at(i + 1)) ++ans;
-    }
+	vector<bool> warikireru(1000006, false);
+	for (uint j = 0; j < N; j++) {  // N <= 2e5
+		if (j != 0 && A[j] == A[j - 1]) continue;
+		if (j != N - 1 && A[j] == A[j + 1]) warikireru[A[j]] = true;
+		if (A[j] == 1) {
+			std::fill(warikireru.begin() + 2, warikireru.end(), true);
+			break;
+		}
+		for (uint i = 2u; i * A[j] <= 1000000; i++) {
+			warikireru[i * A[j]] = true;
+		}
+	}
 
-    cout << ans << '\n';
-    return 0;
+	uint ans = 0;
+	for (const uint Ai : A) {
+		if (!warikireru[Ai]) ++ans;
+	}
+
+	cout << ans << '\n';
+	return 0;
 }
