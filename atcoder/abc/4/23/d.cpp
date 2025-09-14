9cef8e593caa522ece7b49dd0ed49c729b1b0aa4
u-sho <26079835+u-sho@users.noreply.github.com>
2025-09-14 23:43:15 +0900

    abc423 d; 解説見てuint -> uint64_t とset -> priority_queue

--- atcoder/abc/4/23/d.cpp
+++ atcoder/abc/4/23/d.cpp
@@ -8,30 +8,31 @@ int main() {
 	uint N, K;
 	cin >> N >> K;
 
-	vector<tuple<uint, uint, uint>> ABC(N);
+	vector<tuple<uint64_t, uint, uint>> ABC(N);
 	for (auto &[Ai, Bi, Ci] : ABC) {
 		cin >> Ai >> Bi >> Ci;
 	}
 
-	vector<uint> 入店時刻(N);
-	std::set<std::pair<uint, uint>> 退店時刻人数;
+	vector<uint64_t> 入店時刻(N);
+	using P = pair<uint64_t, uint>;
+	priority_queue<P, vector<P>, greater<P>> 退店時刻人数;
 
 	const auto &[A0, B0, C0] = ABC[0];
-	入店時刻[0]              = A0;
-	uint 店内人数            = C0;
-	退店時刻人数.insert({A0 + B0, C0});
+	退店時刻人数.push({A0 + B0, C0});
+	入店時刻[0]   = A0;
+	uint 店内人数 = C0;
 
 	for (uint i = 1; i < N; i++) {
 		const auto &[Ai, Bi, Ci] = ABC[i];
-		uint 入店時刻i           = max(Ai, 入店時刻[i - 1]);
+		auto 入店時刻i           = max(Ai, 入店時刻[i - 1]);
 		while (!退店時刻人数.empty() && 店内人数 + Ci > K) {
-			const auto [t, n] = *退店時刻人数.begin();
-			退店時刻人数.erase(退店時刻人数.begin());
+			const auto [t, n] = 退店時刻人数.top();
+			退店時刻人数.pop();
 			入店時刻i  = max(t, 入店時刻i);
 			店内人数  -= n;
 		}
 		店内人数 += Ci;
-		退店時刻人数.insert({入店時刻i + Bi, Ci});
+		退店時刻人数.push({入店時刻i + Bi, Ci});
 		入店時刻[i] = 入店時刻i;
 	}
 
