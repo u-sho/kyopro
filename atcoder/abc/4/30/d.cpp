c4680a9446e25721076db108b3e9b764e244a2cc
u-sho <26079835+u-sho@users.noreply.github.com>
2025-11-03 09:59:30 +0900

    abc430 d; 解説ac

--- atcoder/abc/4/30/d.cpp
+++ atcoder/abc/4/30/d.cpp
@@ -1,11 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <atcoder/all>
-using namespace atcoder;
-
-#define REP(i, n)  for (size_t i = 0UL; i < (n); i++)
-#define REP1(i, n) for (size_t i = 1UL; i < (n); i++)
+// #define DEBUG
 
 int main() {
 	cin.tie(nullptr);
@@ -15,20 +11,43 @@ int main() {
 	cin >> N;
 	vector<uint> X(N);
 	for (auto& Xi : X) cin >> Xi;
-	ranges::sort(X);
 
-	if (N == 1) {
-		cout << "0\n";
-		return 0;
-	}
+	set<uint> x;
+	x.insert(0);
+	uint64_t sum_d = UINT_MAX;
+#ifdef DEBUG
+	printf("  prev_di       di      next_di        sum_d\n");
+#endif
+	for (const uint Xi : X) {
+		x.insert(Xi);
 
-	uint64_t sum = 0;
-	for (uint i = 0; i < N; i++) {
-		if (i == 0) sum += X[i + 1] - X[i];
-		else if (i == N - 1) sum += X[i] - X[i - 1];
-		else sum += min(X[i] - X[i - 1], X[i + 1] - X[i - 1]);
+		const auto it_prev = --x.lower_bound(Xi);
+		const auto it_next = x.upper_bound(Xi);
+		const uint prev_Xi = *it_prev;
+		const uint next_Xi = (it_next != x.end() ? *it_next : UINT_MAX);
+
+		const uint di = min(next_Xi - Xi, Xi - prev_Xi);
+
+		const uint prevprev_Xi = (it_prev != x.begin() ? *ranges::prev(it_prev) : 0);
+		const uint nextnext_Xi = (it_next != x.end() ? *ranges::next(it_next) : UINT_MAX);
+
+		const uint prev_di_old = prev_Xi ? min(prev_Xi - prevprev_Xi, next_Xi - prev_Xi) : next_Xi;
+		const uint prev_di     = prev_Xi ? min(prev_Xi - prevprev_Xi, /**/ Xi - prev_Xi) : /**/ Xi;
+
+		const uint next_di_old = min(nextnext_Xi - next_Xi, next_Xi - prev_Xi);
+		const uint next_di     = min(nextnext_Xi - next_Xi, next_Xi - /**/ Xi);
+
+		sum_d += di;
+		sum_d -= prev_di_old - prev_di;
+		sum_d -= next_di_old - next_di;
+#ifdef DEBUG
+		printf("pp: %u, p: %u, Xi: %u, n: %u, nn: %u\n", prevprev_Xi, prev_Xi, Xi, next_Xi,
+		       nextnext_Xi);
+		printf("%11u%11u%11u%18lu\n", prev_di, di, next_di, sum_d);
+#else
+		cout << sum_d << "\n";
+#endif
 	}
-	cout << sum << "\n";
 
 	return 0;
 }
