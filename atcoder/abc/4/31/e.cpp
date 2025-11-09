87de2add4e5241429639bb2527db519fe9fecabc
u-sho <26079835+u-sho@users.noreply.github.com>
2025-11-10 00:01:51 +0900

    wip. 解説チラ見

--- atcoder/abc/4/31/e.cpp
+++ atcoder/abc/4/31/e.cpp
@@ -1,22 +1,36 @@
 /**
- *
- * 両側から進ませる？
- *
- * 外枠は，S/G以外光が届いてはいけない．
- * 内枠は，縦線と横線は，カットしたときの横切る光が奇数になるように
- * 
- * test3: 5
- * 
- * //  \ /   
- *    \ / // 
- * \ /  \/\\ 
- *  ///  /// 
- * //    //\ 
- *   /\\ //  
- * \/// /\\ \
- * /\\/  ////
- * /\\//\/\/ 
- * \\ / \\ //
- * 
+ * A: T-B, L-R
+ * B: T-R, L-B
+ * C: R-B, L-T
+ * の無向グラフ．辺のコストを0とする．
+ * なお，A,B,Cの付け替えコストを1とする．
  */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int T, H, W;
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+
+	for (cin >> T; T--;) {
+		cin >> H >> W;
+		vector<string> S(H);
+		for (auto& Si : S) cin >> Si;
+
+		vector<vector<uint>> c(H + 1, vector<uint>(2u * (W + 1u), 0));
+		// c[i][j]: i行目, j/2列目の j%2 ? T : L.
+		// Start: c[0][0], Goal: c[H-1][2*W+1]
+		for (uint i = 0; i < H; i++) {
+			for (uint j = 0; j < 2u * (W + 1u); j++) {
+				if (j % 2) {
+					// T
+				}
+			}
+		}
+	}
+
+	return 0;
+}
