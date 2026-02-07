467c41938b16246ebfa164f6d52dff11a15dde0c
u-sho <26079835+u-sho@users.noreply.github.com>
2026-02-08 03:45:12 +0900

    abc444 d; 解説ac

--- atcoder/abc/4/44/d.cpp
+++ atcoder/abc/4/44/d.cpp
@@ -1,16 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	cin.tie(nullptr);
-	ios::sync_with_stdio(false);
+#define divmod(n, d) make_pair((n) / (d), (n) % (d))
 
+int main() {
 	uint N;
 	cin >> N;
 	vector<uint> A(N);
 	for (uint& Ai : A) cin >> Ai;
 	ranges::sort(A);
 
-	vector<uint> ans = {0};
-	for (const auto) return 0;
+	vector ans = {0ul};
+	for (uint i = 0; i < N; i++) {
+		if (i > 0 && A[i] == A[i - 1]) continue;
+		for (uint j = ans.size(); j <= A[i]; j++) {
+			const auto& [x, y] = divmod(ans.back() + N - i, uint64_t(10));
+			ans.back()         = y;
+			ans.push_back(x);  // 最大 6e15 くらい？
+		}
+	}
+
+	if (ans.size() > 1 && ans.back() == 0) ans.pop_back();
+	for_each(ans.rbegin(), ans.rend(), [](auto ci) { cout << ci; });
+
+	return 0;
 }
